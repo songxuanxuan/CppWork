86be1e0818b923869bc535038cd22a3c694e05e3
xuan <xuan@DESKTOP-OVKHHOH>
2020-06-14 18:32:48 +0800

    write a example for dll

---
+++ DllInject/DllInject.cpp
@@ -0,0 +1,22 @@
+﻿// DllInject.cpp : 定义应用程序的入口点。
+//
+
+#include <Windows.h>
+#include <stdio.h>
+#include <synchapi.h>
+
+
+
+int  main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+
+        printf("+++++++++++ \n");
+        Sleep(1000);
+    }
+    getchar();
+}
+
+
+
